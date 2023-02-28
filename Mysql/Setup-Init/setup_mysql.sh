09a21c41ec97549baabf1e0c4661b20793827d0b
storm1945 <85239629+storm1945@users.noreply.github.com>
2023-02-28 16:15:35 +0800

    2023-02-28 16:13:47

--- Mysql/Setup-Init/setup_mysql.sh
+++ Mysql/Setup-Init/setup_mysql.sh
@@ -3,43 +3,32 @@
 
 # file_key file_sec file_line_num
 function anaylize_config_file(){
-	local filepath=$1
-	#得到区块数组
-	local g_sec=(`sed -n '/\[*\]/p' $filepath |grep -v '^#'|tr -d []`)
-	#sed -n '/\[*\]/p' 得到包含[*]的行
-	#grep -v '^#' 去掉#打头的行
-	#tr -d [] 去掉[]
-	#g_sec=(client mysqld mysqld_safe)
-	local i=0 j=0 k=0
-	for ((i=0;i<${#g_sec[@]};i++))
+#得到区块数组
+local g_sec=(`sed -n '/\[*\]/p' $1 |grep -v '^#'|tr -d []`)
+local g_sec_num=(`sed -n '/\[*\]/p' $1 |grep -v '^#'|awk '{print NR}'`)
+#sed -n '/\[*\]/p' 得到包含[*]的行
+#grep -v '^#' 去掉#打头的行
+#tr -d [] 去掉[]
+#g_sec=(client mysqld mysqld_safe)
+
+local i=0 j=0 k=0
+for ((i=0;i<${#g_sec[@]};i++))
+do
+local g_names=(`sed -n '/\['${g_sec[i]}'\]/,/\[/p' $1|grep -Ev '\[|\]|^$|^#'|awk -F '=' '{print $1}'`)
+#sed -n '/\['${g_sec[i]}'\]/,/\[/p' 得到从[${g_sec[i]}]到临近[的所有行
+#grep -Ev '\[|\]|^$|^#' 去掉包含[或]的行 去掉空行 去掉#打头的行
+#awk -F '=' '{print $1}'`得到=号前面字符
+local g_values=(`sed -n '/\['${g_sec[i]}'\]/,/\[/p' $1|grep -Ev '\[|\]|^$|^#'|awk -F '=' '{print $2}'`)
+local g_line_num=(`sed -n '/\['${g_sec[i]}'\]/,/\[/p' $1|grep -Ev '\[|\]|^$|^#'|awk '{print NR}'`)
+#awk -F '=' '{print $1}'`得到=号后面字符
+	for ((j=0;j<${#g_names[@]};j++))
 	do
-		then
-			local sec_name=${g_sec[i]}
-			local k_key=(`sed -n '/\['$sec_name'\]/,/\[/p' $cnf|grep -Ev '\[|\]|^$|^#'|awk -F '=' '{print $1}'`)
-			#sed -n '/\['$sec_name'\]/,/\[/p' 得到从[$sec_name]到临近[的所有行
-			#grep -Ev '\[|\]|^$|^#' 去掉包含[或]的行 去掉空行 去掉#打头的行
-			#awk -F '=' '{print $1}'`得到=号前面字符
-			local k_value=(`sed -n '/\['$sec_name'\]/,/\[/p' $cnf|grep -Ev '\[|\]|^$|^#'|awk -F '=' '{print $2}'`)
-			#awk -F '=' '{print $1}'`得到=号后面字符
-			for ((j=0;j<${#k_key[@]};j++))
-			do	
-				file_key[k]=sec_name
-				file_sec[k]=k_value[j]
-				k++
-			done
+	echo ${g_names[$j]}" "${g_values[$j]}" "`expr ${g_line_num[$j]} + ${g_sec_num[i]}`
 	done
+done
 }
-function get_value_by_key(){
+anaylize_config_file "/etc/my.cnf"
 
-
-}
-
-
-}
-	if [ ${g_sec[i]} == "mysql57-community" ] 
-	elif  [ ${g_sec[i]} == "mysql80-community" ]
-	then
-	fi
 mkdir /downloads
 cd downloads
 wget https://dev.mysql.com/get/mysql80-community-release-el7-7.noarch.rpm
@@ -47,11 +36,6 @@ if [ ! -e "/downloads/mysql80-community-release-el7-7.noarch.rpm"]
 	exit
 fi
 yum localinstall -y mysql80-community-release-el7-7.noarch.rpm
-
-
-
-
-
 vim /etc/yum.repos.d/mysql-community.repo
 yum install -y mysql-community-server
 useradd -s /sbin/nologin mysql

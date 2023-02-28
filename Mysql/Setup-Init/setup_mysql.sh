3d8a48d008733c9d941d9f5d63cd3721022a773e
storm1945 <85239629+storm1945@users.noreply.github.com>
2023-02-28 16:19:53 +0800

    -

--- Mysql/Setup-Init/setup_mysql.sh
+++ Mysql/Setup-Init/setup_mysql.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 wget https://dev.mysql.com/get/mysql80-community-release-el7-7.noarch.rpm
 if [ ! -e "/downloads/mysql80-community-release-el7-7.noarch.rpm"]
-	exit
+	exit 0
 fi
 yum localinstall -y mysql80-community-release-el7-7.noarch.rpm
-vim /etc/yum.repos.d/mysql-community.repo
+
 yum install -y mysql-community-server
 useradd -s /sbin/nologin mysql
